934cf05da69c458c3f88a500c145a61c2883d134
Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>
2021-08-26 01:21:52 +0530

    [NEW] Create/List/Update/Delete URLs Endpoint (#6)
    * added create and read all url endpoint
    
    * format with black
    
    * add slug field to url model
    
    * added slug
    
    * get url by pk and slug working
    
    * generate random string'
    
    * remove utils from django apps
    
    * format with black
    
    * add clicks field

--- server/urls/models.py
+++ server/urls/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from utils.generate_random_string import generate_random_string
+
+
 class Url(models.Model):
     name = models.CharField(blank=True, max_length=100)
+    slug = models.CharField(blank=False, unique=True, max_length=100)
     description = models.TextField(blank=True)
     location = models.URLField(max_length=256)
+    clicks = models.IntegerField(default=0,db_index=True)
     boosted = models.BooleanField(verbose_name="is_boosted", default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = generate_random_string()
+        super().save(*args, **kwargs)
--- server/urls/views.py
+++ server/urls/views.py
@@ -1,7 +1,24 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from urls.serializers import UrlSerializer
+from urls.models import Url
 
 
-class URLView(APIView):
-    def get(self, _request):
-        return Response(data={"health": "working"})
+class UrlListCreateView(ListCreateAPIView):
+    queryset = Url.objects.all()
+    serializer_class = UrlSerializer
+
+
+class UrlDetailBase(RetrieveUpdateDestroyAPIView):
+    queryset = Url.objects.all()
+    serializer_class = UrlSerializer
+
+
+class UrlDetailPrimaryKey(UrlDetailBase):
+    lookup_field = "pk"
+
+
+class UrlDetailSlug(UrlDetailBase):
+    lookup_field = "slug"
