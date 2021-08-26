57dbc4a0ee2c8ded567b7b8ab76c223b317e1470
Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>
2021-08-26 13:09:15 +0530

    FIX: allowed hosts for django server (#9)

--- server/murls_server/settings/__init__.py
+++ server/murls_server/settings/__init__.py
@@ -9,7 +9,7 @@ IS_DEV = getenv("IS_DEV") is not None and getenv("IS_DEV").upper() == "TRUE"
 if IS_DEV:
     from .dev import *
 else:
-    print("Running in PRODUCTION Environment")
+    print("Congratulation! MURLs Server is now running 🎉")
     from .production import *
 
 from .base import *
