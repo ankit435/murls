33517ba5168abda1f2621958e0d73abcc6856ddc
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-27 00:37:55 +0530

    add migration

--- server/murls_server/settings/dev.py
+++ server/murls_server/settings/dev.py
@@ -4,9 +4,9 @@ from .base import BASE_DIR
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'cc9a-2405-201-a003-c1ae-d980-c3d2-3126-9dc9.ngrok.io'
+    "localhost",
+    "127.0.0.1",
+    "cc9a-2405-201-a003-c1ae-d980-c3d2-3126-9dc9.ngrok.io",
 ]
 
 SECRET_KEY = "django-insecure-r^_0hj4#ekqyqkx^muo@ls(8+91125b@38p9dz1a!ec7yko49v"
