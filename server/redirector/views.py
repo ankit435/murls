05e997f4df74837a141c3aee032b663ad0e05583
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-27 13:52:20 +0530

    cached_locatgion from redis

--- server/redirector/views.py
+++ server/redirector/views.py
@@ -5,12 +5,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from urls.models import Url
+from utils.redis.Client import Redis
+
+redis = Redis()
 class Redirector(APIView):
-    def get(self, request:HttpRequest, slug:str)->QuerySet[Url]:
+    def get(self, request: HttpRequest, slug: str) -> QuerySet[Url]:
+        
+        cached_location = redis.get(slug)
+        if cached_location is not None:
+            return HttpResponseRedirect(cached_location)
+            
         try:
-            found_url = Url.objects.values('location').get(slug=slug)
+            found_url = Url.objects.values("location").get(slug=slug)
             return HttpResponseRedirect(found_url.get("location"))
         except:
-            return HttpResponseRedirect('/')
-        
-        
+            return HttpResponseRedirect("/")
