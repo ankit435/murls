af56202bc9d4f9e010f407406e277a0161b9f00e
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-27 15:17:09 +0530

    update script

--- server/murls_server/settings/dev.py
+++ server/murls_server/settings/dev.py
@@ -7,6 +7,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "cc9a-2405-201-a003-c1ae-d980-c3d2-3126-9dc9.ngrok.io",
+    "ad5e-2405-201-a003-c1ae-bd13-72b4-86a3-4026.ngrok.io"
 ]
 
 SECRET_KEY = "django-insecure-r^_0hj4#ekqyqkx^muo@ls(8+91125b@38p9dz1a!ec7yko49v"
