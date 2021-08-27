12041f0558db3715c3d55095242bff7989b0018a
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-27 12:17:47 +0530

    redirector now working

--- server/redirector/views.py
+++ server/redirector/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
+from django.http import HttpResponseRedirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from urls.models import Url
+class Redirector(APIView):
+    def get(self, request:HttpRequest, slug:str)->QuerySet[Url]:
+        try:
+            found_url = Url.objects.values('location').get(slug=slug)
+            return HttpResponseRedirect(found_url.get("location"))
+        except:
+            return HttpResponseRedirect('/')
+        
+        
