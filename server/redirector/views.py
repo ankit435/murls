30227b2754bf40bc578daa29118a3796eef09c7d
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-28 11:35:57 +0530

    permenanent redirect

--- server/redirector/views.py
+++ server/redirector/views.py
@@ -1,6 +1,6 @@
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
-from django.http import HttpResponseRedirect
+from django.http import HttpResponsePermanentRedirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -16,10 +16,10 @@ class Redirector(APIView):
         cached_location = redis.get(slug)
 
         if cached_location is not None:
-            return HttpResponseRedirect(cached_location)
+            return HttpResponsePermanentRedirect(cached_location)
 
         try:
             found_url = Url.objects.values("location").get(slug=slug)
-            return HttpResponseRedirect(found_url.get("location"))
+            return HttpResponsePermanentRedirect(found_url.get("location"))
         except:
-            return HttpResponseRedirect("/")
+            return HttpResponsePermanentRedirect("/")
