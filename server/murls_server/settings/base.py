1a5d9b3f56e38e35d6460abce71ef066a7d35d52
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-30 01:40:00 +0530

    removed auth password validators

--- server/murls_server/settings/base.py
+++ server/murls_server/settings/base.py
@@ -69,25 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "murls_server.wsgi.application"
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
