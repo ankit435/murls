a5d6c4e382742faae7545515057db8f3ab92017e
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-08-30 03:04:11 +0530

    cache auth user in redis

--- server/utils/auth/auth0.py
+++ server/utils/auth/auth0.py
@@ -3,9 +3,11 @@ from django.http import HttpRequest
 from django.contrib.auth import authenticate as authenticate_user
 from rest_framework.authentication import BaseAuthentication
 
+from utils.redis.Client import Redis
 from .get_user import get_user
+from .cache_auth_user import cache_auth_user
 
-
+redis = Redis()
 class Auth0Authentication(BaseAuthentication):
     def authenticate(self, request: HttpRequest):
         authorization_header = request.headers.get("Authorization")
@@ -15,10 +17,16 @@ class Auth0Authentication(BaseAuthentication):
             user = authenticate_user(remote_user="blue")
             return (user, None)
 
-        auth_user_email = get_user(authorization_header)
+        cached_auth_user = redis.get(authorization_header)
+
+        if cached_auth_user is not None:
+            return (authenticate_user(remote_user=cached_auth_user),None)
+
+        # auth_user_email = get_user(authorization_header)
+        auth_user_email = authorization_header
+
+        cache_auth_user(authorization_header,auth_user_email)
 
         user = authenticate_user(remote_user=auth_user_email)
+
         return (user, None)
-
-
-# TODO: cache in redis
---
+++ server/utils/auth/cache_auth_user.py
@@ -0,0 +1,12 @@
+from threading import Thread
+
+from utils.redis.Client import Redis
+
+
+def run_in_thread(authorization_header:str, auth_user_email:str):
+    """run inside thread to store the data in background"""
+    redis = Redis()
+    redis.set_with_expiry(authorization_header,auth_user_email,5*60)
+
+def cache_auth_user(authorization_header:str, auth_user_email:str):
+    Thread(target=run_in_thread,args=(authorization_header,auth_user_email),daemon=True).start()
