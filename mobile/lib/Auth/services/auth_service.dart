7c7c1d66414fdda88b8fb77cf555033414d1957c
ankit435 <ankitkumarsingh435@gmail.com>
2021-08-29 16:10:42 +0530

    add search

--- mobile/lib/Auth/services/auth_service.dart
+++ mobile/lib/Auth/services/auth_service.dart
@@ -54,13 +54,11 @@ class AuthService {
         scopes: ['openid', 'profile', 'offline_access', 'email'],
         promptValues: ['login'],
       );
-      print('hi');
 
       final AuthorizationTokenResponse? result =
           await appAuth.authorizeAndExchangeCode(
         authorizationTokenRequest,
       );
-      print(result);
 
       return await _setLocalVariables(result);
       // } on PlatformException {
@@ -108,7 +106,6 @@ class AuthService {
     final bool isValidResult =
         result != null && result.accessToken != null && result.idToken != null;
 
-    print(result.accessToken);
     if (isValidResult) {
       auth0AccessToken = result.accessToken;
 
