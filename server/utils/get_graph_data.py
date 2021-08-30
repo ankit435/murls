c2cecc6f1e6a003390851b1e76342a452b795abd
Aditya Mitra <55396651+aditya-mitra@users.noreply.github.com>
2021-08-30 18:08:53 +0530

    [FIX] `get_graph_data` giving 500 response (#36)
    `TruncDate` does not seem to work

--- server/utils/get_graph_data.py
+++ server/utils/get_graph_data.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
-from django.utils import timezone
 from django.db.models import QuerySet, Count
 from django.db.models.functions import TruncMinute, TruncHour, TruncDate, TruncDay
+from django.utils import timezone
 
 from urls.models import UrlTrack
 
@@ -10,7 +10,7 @@ def get_graph_data(url_id: int, query: str):
     if not query:
         query = " "
 
-    query = query.lower()
+    query = query.lower().replace(" ", "_")
 
     if query == "today_minute":
         return (
@@ -33,23 +33,23 @@ def get_graph_data(url_id: int, query: str):
             .all()
         )
 
-    elif query == "last 7 days" or query == "7 days":
+    elif query == "last_7_days" or query == "7_days":
         return (
             QuerySet(model=UrlTrack)
             .filter(url=url_id)
             .filter(when__gte=timezone.now() - timedelta(days=7))
-            .annotate(on=TruncDate("when"))
+            .annotate(on=TruncDay("when",))
             .values("on")
             .annotate(count=Count("id"))
             .all()
         )
 
-    elif query == "last month" or query == "last 30 days":
+    elif query == "last_month" or query == "last_30_days":
         return (
             QuerySet(model=UrlTrack)
             .filter(url=url_id)
             .filter(when__gte=timezone.now() - timedelta(days=30))
-            .annotate(on=TruncDate("when"))
+            .annotate(on=TruncDay("when"))
             .values("on")
             .annotate(count=Count("id"))
             .all()
@@ -66,6 +66,7 @@ def get_graph_data(url_id: int, query: str):
         )
 
     elif query == "day":
+
         return (
             QuerySet(model=UrlTrack)
             .filter(url=url_id)
@@ -78,7 +79,8 @@ def get_graph_data(url_id: int, query: str):
     return (
         QuerySet(model=UrlTrack)
         .filter(url=url_id)
-        .annotate(on=TruncDate("when"))
+        .annotate(on=TruncDay("when"))
         .values("on")
         .annotate(count=Count("id"))
+        .all()
     )
