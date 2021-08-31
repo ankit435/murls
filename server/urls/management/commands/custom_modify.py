7351d0db554b228b0ce4f1aa6a9202ba000baf12
aditya-mitra <ultrapokeinstinct@gmail.com>
2021-09-01 02:43:10 +0530

    fix the logging

--- server/urls/management/commands/custom_modify.py
+++ server/urls/management/commands/custom_modify.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import QuerySet
 from django.contrib.auth import get_user_model
 
-from urls.models import Url
+from urls.models import Url,RecycleUrl
 
 
 class Command(BaseCommand):
@@ -12,5 +12,5 @@ class Command(BaseCommand):
         User = get_user_model()
         blue_user = User.objects.get(username="blue")
         QuerySet(model=Url).filter(creator=blue_user).filter(id__gt=144).delete()
-
+        QuerySet(model=RecycleUrl).filter(creator=blue_user).delete()
         self.stdout.write("Deleteing those entries")
